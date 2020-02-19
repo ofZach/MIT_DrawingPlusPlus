1b42ec00d64a73bee8bdc8f6cd19eb0489f3539c
velouria <montoyamoraga@gmail.com>
2020-02-19 00:14:08 -0500

    add p5.js basicDrawing3

--- code_p5js/basicDrawing3/js/sketch.js
+++ code_p5js/basicDrawing3/js/sketch.js
@@ -1,126 +1,78 @@
-// // declare array called polyline
-// let polyline = [];
+// declare arrays for storing lines
+let polyline = [];
+let resampled = [];
 
-// function setup() {
+function setup() {
 
-//   // create canvas
-//   createCanvas(1024, 768);
+  // create canvas
+  createCanvas(1024, 768);
 
-//   // white stroke
-//   stroke(255);
+  // noStroke, red fill
+  noStroke();
+  fill(255, 0, 0);
+}
 
-// }
+function draw() {
+  // black background
+  background(0, 0, 0);
 
-// function draw() {
+    for (let i = 0; i < resampled.length; i++) {
+    ellipse(resampled[i].x, resampled[i].y, 20, 20);
+  }
+}
 
-//   // black background
-//   background(0, 0, 0);
+// while mouse is dragged, create PVector with current mouse position and add to polyline
+function mouseDragged() {
 
-//     // go through every vector element in the array polyline
-//     for (let i = 0; i < polyline.length; i++) {
-//       // for all elements except for the last one
-//       if (i + 1 < polyline.length) {
-//         // draw a line between current element and the next one
-//         line(polyline[i].x, polyline[i].y, polyline[i+1].x, polyline[i+1].y);
-//       }
-//     }
-// }
+  polyline.push(createVector(mouseX, mouseY));
 
-// // while mouse is dragged
-// // create vector with current mouse position and add to polyline
-// function mouseDragged() {
-//   polyline.push(createVector(mouseX, mouseY));
-// }
-
-// // when mouse is pressed, clear polyline
-// function mousePressed() {
-//   if (polyline.length > 0) {
-//     polyline = [];
-//   }
-// }
-
-
-// // declare polyline, an ArrayList made out of PVector elements
-// ArrayList<PVector> polyline = new ArrayList<PVector>();
-
-// // declare resampled, an ArrayList made out of PVector elements
-// ArrayList<PVector> resampled = new ArrayList<PVector>();
-
-// void setup() {
-
-//   // create canvas
-//   size(1024, 768);
-
-//   // noStroke, red fill
-//   noStroke();
-//   fill(255, 0, 0);
-// }
-
-// void draw() {
-
-//   // black background
-//   background(0, 0, 0);
-
-//   for (int i = 0; i < resampled.size(); i++) {
-//     ellipse(resampled.get(i).x, resampled.get(i).y, 20, 20);
-//   }
-// }
-
-// // while mouse is dragged, create PVector with current mouse position and add to polyline
-// void mouseDragged() {
-
-//   polyline.add(new PVector(mouseX, mouseY));
-
-//   //println("polyline.size() : " + polyline.size());
-//   //println("resampled.size() : " + resampled.size());
-
-//   // if resampled is empty, add first element
-//   if (resampled.size() == 0) {
-//     resampled.add(new PVector(polyline.get(0).x, polyline.get(0).y));
-//   }
-//   // in other case
-//   else {
-
-//     int resampledLast = resampled.size() - 1;
-  
-//     // x1, y1 is newest PVector in resampled 
-//     float x1 = resampled.get(resampledLast).x;
-//     float y1 = resampled.get(resampledLast).y;
-
-//     // x2, y2 is last PVector in polyline 
-//     float x2 = polyline.get(polyline.size() - 1).x;
-//     float y2 = polyline.get(polyline.size() - 1).y;
-    
-//     // variable for storing the distance between x1,y1 and x2,y2
-//     float measure = 0;
-
-//     if (x1 != x2 && y1 != y2) {
-//       measure = dist(x1, y1, x2, y2);
-//     }
-
-//     // if distance is bigger than 20, append new PVector to resampled
-//     if (measure > 20) {
+    // if resampled is empty, add first element
+  if (resampled.length == 0) {
+    resampled.push(createVector(polyline[0].x, polyline[0].y));
+  }
+  // in other case
+  else {
       
-//       // calculate angle
-//       float theta = atan2(y2 - y1, x2 - x1);
+    let resampledLast = resampled.length - 1;
 
-//       // calculate x,y of new PVector in resampled
-//       float newX = x1 + 20 * cos(theta);
-//       float newY = y1 + 20 * sin(theta);
+    // x1, y1 is newest PVector in resampled 
+    let x1 = resampled[resampledLast].x;
+    let y1 = resampled[resampledLast].y;
 
-//       resampled.add(new PVector(newX, newY));
-//     }
-//   }
-// }
+    // x2, y2 is last PVector in polyline 
+    let x2 = polyline[polyline.length - 1].x;
+    let y2 = polyline[polyline.length - 1].y;
+    
+    // variable for storing the distance between x1,y1 and x2,y2
+    let measure = 0;
 
-// // when mouse is pressed, clear polyline
-// void mousePressed() {
+    if (x1 != x2 && y1 != y2) {
+      measure = dist(x1, y1, x2, y2);
+    }
 
-//   while (polyline.size() > 0) {
-//     polyline.remove(0);
-//   }
+    // if distance is bigger than 20, append new PVector to resampled
+    if (measure > 20) {
+        
+        // calculate angle
+        let theta = atan2(y2 - y1, x2 - x1);
 
-//   while (resampled.size() > 0) {
-//     resampled.remove(0);
-//   }
-// }
+        // calculate x,y of new PVector in resampled
+        let newX = x1 + 20 * cos(theta);
+        let newY = y1 + 20 * sin(theta);
+
+        resampled.push(createVector(newX, newY));
+    }
+  }
+}
+
+// when mouse is pressed, clear lines
+function mousePressed() {
+  if (polyline.length > 0) {
+    polyline = [];
+  }
+  if (resampled.length > 0) {
+    resampled = [];
+  }
+}
+  
+
