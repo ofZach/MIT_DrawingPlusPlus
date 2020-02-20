6c8dd6dbc90945396a5285f1bb30df508d76eeec
montoyamoraga <montoyamoraga@gmail.com>
2020-02-19 20:51:23 -0500

    add comments to drawing3d

--- code/drawing3d/src/ofApp.cpp
+++ code/drawing3d/src/ofApp.cpp
@@ -12,31 +12,50 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    
+    // set thickness of ofPolyline
     ofSetLineWidth(3);
     
+    // only add a new element to ofPolyline if mouse is pressed
     if (bMousePressed == true){
         line.addVertex( mouseX, mouseY);
     }
     
-    
+    // go through every element in ofPolyline line
     for (int i = 0; i < line.size(); i++){
-        line[i] -= ofPoint( ofGetWidth()/2, ofGetHeight()/2);
+        // each point is translated by (-width/2, -height/2)
+         line[i] -= ofPoint( ofGetWidth()/2, ofGetHeight()/2);
     }
     
+    // declare ofMatrix4x4 mat, an instance of a matrix
     ofMatrix4x4 mat;
+    
+    // populate ofMatrix4x4 with a rotational matrix
+    // 4 arguments: angle, x, y, z
+    // (1, 0, 1, 0) means rotate in 1 degree along the y axis
+    // x axis is the horizontal one of the screen plane, direction: right
+    // y axis is the vertical one of the screen plane, direction: downwards
+    // z axis is perpendicular to screen, direction: inside of screen
     mat.makeRotationMatrix(1, 0, 1, 0);
+    
+    // go through every element in ofPolyline line
     for (int i = 0; i < line.size(); i++){
+        // create copy of current element in ofPolyline line
         ofPoint pt = line[i];
+        // use rotation matrix to rotate this point
         pt = mat * pt;
-        line[i] = pt;
+        // replace old value of element with the new rotated one
+         line[i] = pt;
     }
     
-    
+    // go through every element in ofPolyline line
     for (int i = 0; i < line.size(); i++){
-        line[i] += ofPoint( ofGetWidth()/2, ofGetHeight()/2);
+        // undo original translation
+        // move each point by (width/2, height/2)
+         line[i] += ofPoint( ofGetWidth()/2, ofGetHeight()/2);
     }
     
+    // draw ofPolyline line
     line.draw();
 }
 
@@ -58,20 +77,23 @@ void ofApp::mouseMoved(int x, int y ){
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
 
-    //line.addVertex(x,y);
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
-    bMousePressed = true;
-    line.clear();
     
+    // when mousePressed, set boolean to true
+    bMousePressed = true;
+    
+    // delete all elements in ofPolyline line
+    line.clear();
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
- bMousePressed = false;
+    
+    // when mouseReleased, set boolean to false
+    bMousePressed = false;
 }
 
 //--------------------------------------------------------------
