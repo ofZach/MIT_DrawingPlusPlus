41114ec6b5a85a5beea40b103ac3782fbf80213b
zach lieberman <zach@sfpc.io>
2020-04-13 12:08:05 -0400

    drawing example for cindy

---
+++ code/CLDtoDrawing/src/ofApp.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+#include "ofxCv.h"
+
+class ofApp : public ofBaseApp{
+public:
+	void setup();
+	void update();
+	void draw();
+	
+	ofImage input, output, canny;
+	
+    ofParameter<float> sigma1, sigma2, tau;
+    ofParameter<int> halfw, smoothPasses, black, thresh, cannyParam1, cannyParam2;
+    ofParameter<bool> doFDoG, doThresh, doThin, doCanny;
+    
+	ofxPanel gui;
+    
+    ofVideoGrabber grabber;
+    ofImage img;
+    
+    
+};
