4351b25d1032a9a565261d3ceeb098521361eba2
velouria <montoyamoraga@gmail.com>
2020-04-14 09:44:07 -0400

    add comments to angleLengthLine

--- code/angleLengthLine/src/ofApp.cpp
+++ code/angleLengthLine/src/ofApp.cpp
@@ -2,8 +2,9 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
-     ofBackground(0,0,0);
+    
+    // paint the background black
+    ofBackground(0,0,0);
 }
 
 //--------------------------------------------------------------
@@ -13,19 +14,23 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    
+    // draw the ofPolyline line
     line.draw();
     
-    
+    // go through every angleLengthline in the alines vector
     for (int i = 0; i < alines.size(); i++){
-        
+        // draw the corresponding instance of angleLengthline
         alines[i].draw();
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
+    // when any key is pressed
+    
+    // clear all the angleLengthLine elements in the alines vector
     alines.clear();
 }
 
@@ -41,22 +46,39 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
+    
+    // every time the mouse is dragged
+    
+    // add vertex to ofPolyline line with the mouse position
     line.addVertex(ofPoint(x,y));
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
+    // every time the mouse is pressed
+    
+    // delete all vertices in ofPolyline line
     line.clear();
+    // add new vertex to ofPolyline with current mouse position
     line.addVertex(ofPoint(x,y));
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
     
+    // every time the mouse is released
+    
+    // declare new instance of angleLengthLine
     angleLengthLine angleLine;
+    
+    // calculate from line, the vectors inside of angleLine
     angleLine.convertFrom(line);
+    
+    // add the angleLengthLine angleLine to the alines vector
     alines.push_back(angleLine);
+    
+    // clear all vertices in ofPolyline line
     line.clear();
 }
 
